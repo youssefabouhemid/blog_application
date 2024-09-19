c66ec404c50fe3a1b37adc7c98e2a849ac766109
Youssef Ahmad Abouhemid <63516733+greatyassoo@users.noreply.github.com>
2024-09-19 20:10:55 +0300

    signup, login, logout functional
    created new user serializer to serialize the data.
    
    added logic to controllers
    
    added session bug fix moduel

---
+++ app/controllers/concerns/rack_session_fix.rb
@@ -0,0 +1,18 @@
+
+# some weird bug in devise and rails 7 where it forces session auth even though its not used.
+# This is the fix workaround by creating a fake session hash
+module RackSessionFix
+  extend ActiveSupport::Concern
+  class FakeRackSession < Hash
+    def enabled?
+      false
+    end
+  end
+  included do
+    before_action :set_fake_rack_session_for_devise
+    private
+    def set_fake_rack_session_for_devise
+      request.env['rack.session'] ||= FakeRackSession.new
+    end
+  end
+end
