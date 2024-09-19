8662a474c22dcd63a77114c7122fb50ead3fc879
Youssef Ahmad Abouhemid <63516733+greatyassoo@users.noreply.github.com>
2024-09-19 19:35:32 +0300

    add devise jwt configuration

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -310,4 +310,18 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+
+  # on successful login attach jwt token to Authorization header,
+  # on logout revoke the jwt token,
+  # jwt configs: 30 minutes expiry
+  config.jwt do |jwt|
+    jwt.secret = Rails.application.credentials.fetch(:secret_key_base)
+    jwt.dispatch_requests = [
+      ['POST', %r{^/login$}]
+    ]
+    jwt.revocation_requests = [
+      ['DELETE', %r{^/logout$}]
+    ]
+    jwt.expiration_time = 30.minutes.to_i
+  end
 end
