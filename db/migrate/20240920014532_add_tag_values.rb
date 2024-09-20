85e31bbdf641ad5c2ce89775e3615cc9c9e2dac3
Youssef Ahmad Abouhemid <63516733+greatyassoo@users.noreply.github.com>
2024-09-20 04:50:15 +0300

    add insert tag values migration

---
+++ db/migrate/20240920014532_add_tag_values.rb
@@ -0,0 +1,37 @@
+class AddTagValues < ActiveRecord::Migration[7.2]
+  def change
+    Tag.create([
+                 { value: "Test" },
+                 { value: 'Technology' },
+                 { value: 'Programming' },
+                 { value: 'Ruby on Rails' },
+                 { value: 'Web Development' },
+                 { value: 'Startup' },
+                 { value: 'Artificial Intelligence' },
+                 { value: 'Data Science' },
+                 { value: 'Travel' },
+                 { value: 'Food' },
+                 { value: 'Health' },
+                 { value: 'Fitness' },
+                 { value: 'Lifestyle' },
+                 { value: 'Productivity' },
+                 { value: 'Marketing' },
+                 { value: 'Finance' },
+                 { value: 'Investing' },
+                 { value: 'Cryptocurrency' },
+                 { value: 'Design' },
+                 { value: 'Photography' },
+                 { value: 'Mental Health' },
+                 { value: 'Parenting' },
+                 { value: 'Education' },
+                 { value: 'Science' },
+                 { value: 'Innovation' },
+                 { value: 'UX/UI' },
+                 { value: 'Machine Learning' },
+                 { value: 'Self-Improvement' },
+                 { value: 'Social Media' },
+                 { value: 'Environment' },
+                 { value: 'Gaming' }
+               ])
+  end
+end
